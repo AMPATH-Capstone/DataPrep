13b21a2271e09a09e6a50dc860e37f490978d4f1
sydbunch <70718313+sydbunch@users.noreply.github.com>
2020-11-18 14:57:11 -0500

    Update Location_map.R

--- Original-Data/Location_map.R
+++ Original-Data/Location_map.R
@@ -12,7 +12,7 @@ control <- filter(location_data, Treatment =="Control")
 kitale_bounds <- c(34.547910, 0.779964, 35.422696, 1.301727)
 kitale_map <- get_map(location=kitale_bounds, source="google", maptype="roadmap", crop=FALSE)
 ggmap(kitale_map) + 
-  geom_point(data = intervention, aes(x = Long, y = Lat), color = "navy", size = 1) +
+  geom_point(data = intervention, aes(x = Long, y = Lat), color = "blue", size = 1) +
   geom_point(data = control, aes(x = Long, y = Lat), color = "red", size = 1) +
   theme(axis.text.x = element_blank(),
         axis.text.y = element_blank(),
